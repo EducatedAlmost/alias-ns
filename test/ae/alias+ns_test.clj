6acd0c54b6627ea7dea06fbea2577d37ffb67488
AlmostEducated <EducatedAlmost@gmail.com>
2021-11-24 11:07:32 +0000

    Add macro expansion tests

--- test/ae/alias+ns_test.clj
+++ test/ae/alias+ns_test.clj
@@ -2,7 +2,15 @@
   (:require [clojure.test :as t]
             [ae.alias+ns :as sut]))
 
-#_(t/deftest alias+ns-test
-    (t/testing "Aliases can be created by alias+ns."
-      (do (sut/alias+ns 'bar 'ae.bar)
-          (t/is (contains? (ns-aliases 'ae.alias+ns-test) 'bar)))))
+(t/deftest alias+ns-test
+  (t/testing "Aliases can be created by alias+ns."
+    (let [actual (macroexpand `(sut/alias+ns 'bar 'ae.bar))]
+      (t/is (= `(do (create-ns 'ae.bar) (alias 'bar 'ae.bar))
+               actual)))))
+
+(t/deftest aliases+ns-test
+  (t/testing "Multiple aliases can be created by aliases+ns."
+    (let [actual (macroexpand `(sut/aliases+ns {'bar 'ae.bar 'car 'ae.car}))]
+      (t/is (= `(do (sut/alias+ns 'bar 'ae.bar)
+                    (sut/alias+ns 'car 'ae.car))
+               actual)))))
