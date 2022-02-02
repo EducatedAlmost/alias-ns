5766cb00a30b85e7af2a8f464b0fd3780bbc9706
AlmostEducated <EducatedAlmost@gmail.com>
2022-02-02 15:31:35 +0000

    Add reference to 1.11 and CLJ-2123.

--- src/ae/alias+ns.clj
+++ src/ae/alias+ns.clj
@@ -1,9 +1,8 @@
 (ns ae.alias+ns)
 
 (defmacro alias+ns [a ns]
-  `(do
-     (clojure.core/create-ns ~ns)
-     (clojure.core/alias ~a ~ns)))
+  `(do (clojure.core/create-ns ~ns)
+       (clojure.core/alias ~a ~ns)))
 
 (defmacro aliases+ns [m]
   `(do ~@(for [[a ns] m]
